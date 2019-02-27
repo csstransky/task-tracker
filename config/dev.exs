86afbd76d86702fa5c382d1ffeb915dec335843b
csstransky <csstransky@gmail.com>
2019-02-27 14:25:30 -0500

    Giving our database a new password

--- config/dev.exs
+++ config/dev.exs
@@ -62,7 +62,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Configure your database
 config :task_tracker, TaskTracker.Repo,
   username: "task_tracker",
-  password: "iChuseec1Kae",
+  password: "K$42kld2ljf(Ghrdd",
   database: "task_tracker_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,7 @@ config :logger, level: :warn
 # Configure your database
 config :task_tracker, TaskTracker.Repo,
   username: "task_tracker",
-  password: "iChuseec1Kae",
+  password: "K$42kld2ljf(Ghrdd",
   database: "task_tracker_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox

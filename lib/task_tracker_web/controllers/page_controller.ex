60cbaacf3f1391920687b78c837b571931ef8c6c
csstransky <csstransky@gmail.com>
2019-03-14 16:21:26 -0400

    Have underlings displaying correctly on homepage for managers

--- lib/task_tracker_web/controllers/page_controller.ex
+++ lib/task_tracker_web/controllers/page_controller.ex
@@ -5,7 +5,14 @@ defmodule TaskTrackerWeb.PageController do
     if conn.assigns.current_user do
       user = conn.assigns.current_user
       user_tasks = TaskTracker.Tasks.list_user_tasks(user.id)
-      render(conn, "index.html", user_tasks: user_tasks)
+      manager_underlings = TaskTracker.Users.list_manager_underlings(user.id)
+      # I do this to save the HTML from having to use any real logic
+      user_tasks = if length(user_tasks) > 0, do: user_tasks, else: nil
+      manager_underlings = if length(manager_underlings) > 0,
+        do: manager_underlings,
+        else: nil
+      render(conn, "index.html", user_tasks: user_tasks,
+        manager_underlings: manager_underlings)
     else
       render conn, "index.html"
     end
